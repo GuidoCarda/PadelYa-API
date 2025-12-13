a71050c5ef863888e9beb9e55a5bbf5469615076
Guido Cardarelli <guidocarda@hotmail.com>
2025-12-13 15:07:46 -0300

    Fix: added pending migration.

---
+++ Migrations/20251213180323_PendingChanges.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace padelya_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PermissionComponents",
+                keyColumn: "Id",
+                keyValue: 68,
+                column: "Description",
+                value: "Permite ver el catálogo de productos");
+
+            migrationBuilder.InsertData(
+                table: "PermissionComponents",
+                columns: new[] { "Id", "Description", "DisplayName", "ModuleId", "Name", "PermissionType", "RequiredEntity" },
+                values: new object[,]
+                {
+                    { 73, "Permite ver todos los pedidos de clientes", "Ver todos los pedidos", 12, "order:view_all", "Simple", null },
+                    { 74, "Permite cambiar el estado de un pedido", "Actualizar estado de pedido", 12, "order:update_status", "Simple", null },
+                    { 75, "Permite ver el historial de pedidos propios", "Ver pedidos propios", 12, "order:view_own", "Simple", "Player" },
+                    { 76, "Permite realizar una compra (checkout)", "Realizar compra", 12, "order:create", "Simple", "Player" },
+                    { 77, "Permite agregar productos al carrito", "Agregar al carrito", 12, "cart:add", "Simple", "Player" },
+                    { 78, "Permite quitar productos del carrito", "Eliminar del carrito", 12, "cart:remove", "Simple", "Player" },
+                    { 79, "Permite cambiar cantidades en el carrito", "Modificar carrito", 12, "cart:update", "Simple", "Player" },
+                    { 80, "Permite ver el carrito de compras", "Ver carrito", 12, "cart:view", "Simple", "Player" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RolCompositePermission",
+                columns: new[] { "PermissionComponentId", "RoleId" },
+                values: new object[,]
+                {
+                    { 68, 102 },
+                    { 72, 102 },
+                    { 73, 100 },
+                    { 74, 100 },
+                    { 75, 100 },
+                    { 75, 102 },
+                    { 76, 100 },
+                    { 76, 102 },
+                    { 77, 100 },
+                    { 77, 102 },
+                    { 78, 100 },
+                    { 78, 102 },
+                    { 79, 100 },
+                    { 79, 102 },
+                    { 80, 100 },
+                    { 80, 102 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 68, 102 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 72, 102 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 73, 100 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 74, 100 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 75, 100 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 75, 102 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 76, 100 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 76, 102 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 77, 100 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 77, 102 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 78, 100 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 78, 102 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 79, 100 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 79, 102 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 80, 100 });
+
+            migrationBuilder.DeleteData(
+                table: "RolCompositePermission",
+                keyColumns: new[] { "PermissionComponentId", "RoleId" },
+                keyValues: new object[] { 80, 102 });
+
+            migrationBuilder.DeleteData(
+                table: "PermissionComponents",
+                keyColumn: "Id",
+                keyValue: 73);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionComponents",
+                keyColumn: "Id",
+                keyValue: 74);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionComponents",
+                keyColumn: "Id",
+                keyValue: 75);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionComponents",
+                keyColumn: "Id",
+                keyValue: 76);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionComponents",
+                keyColumn: "Id",
+                keyValue: 77);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionComponents",
+                keyColumn: "Id",
+                keyValue: 78);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionComponents",
+                keyColumn: "Id",
+                keyValue: 79);
+
+            migrationBuilder.DeleteData(
+                table: "PermissionComponents",
+                keyColumn: "Id",
+                keyValue: 80);
+
+            migrationBuilder.UpdateData(
+                table: "PermissionComponents",
+                keyColumn: "Id",
+                keyValue: 68,
+                column: "Description",
+                value: "Permite ver todos los productos");
+        }
+    }
+}
